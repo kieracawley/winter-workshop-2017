676e0654c6825665c4bb7604c6fe5136c7d59190
Kiera Cawley <thebluecat544@gmail.com>
2017-02-09 15:11:18 -0500

    It kind of works

---
+++ Final Project/battleship.rb
@@ -0,0 +1,146 @@
+require 'colorize'
+def placeShip(originalBoard)
+	board = originalBoard
+	row = rand(10)
+	coloumn = rand(10)
+	if rand(2) == 0
+		if coloumn < 5
+			board[row][coloumn] = "S".colorize(:light_cyan)
+			board[row][coloumn + 1] = "S".colorize(:light_cyan)
+			board[row][coloumn + 2] = "S".colorize(:light_cyan)
+			board[row][coloumn + 3] = "S".colorize(:light_cyan)
+			board[row][coloumn + 4] = "S".colorize(:light_cyan)
+		else
+			board[row][coloumn] = "S".colorize(:light_cyan)
+			board[row][coloumn - 1] = "S".colorize(:light_cyan)
+			board[row][coloumn - 2] = "S".colorize(:light_cyan)
+			board[row][coloumn - 3] = "S".colorize(:light_cyan)
+			board[row][coloumn - 4] = "S".colorize(:light_cyan)
+		end
+	else
+		if row < 5
+			board[row][coloumn] = "S".colorize(:light_cyan)
+			board[row + 1][coloumn] = "S".colorize(:light_cyan)
+			board[row + 2][coloumn] = "S".colorize(:light_cyan)
+			board[row + 3][coloumn] = "S".colorize(:light_cyan)
+			board[row + 4][coloumn] = "S".colorize(:light_cyan)
+		else
+			board[row][coloumn] = "S".colorize(:light_cyan)
+			board[row - 1][coloumn] = "S".colorize(:light_cyan)
+			board[row - 2][coloumn] = "S".colorize(:light_cyan)
+			board[row - 3][coloumn] = "S".colorize(:light_cyan)
+			board[row - 4][coloumn] = "S".colorize(:light_cyan)
+		end
+	end
+	return board
+end
+def createBoard(ships)
+	newBoard = Array.new()
+	(1..10).each do |index|
+		newBoard.push([])
+		(1..10).each do |index2|
+			newBoard[index - 1].push("O".colorize(:white))
+		end
+	end
+	(0...ships).each do |a|
+		newBoard = placeShip(newBoard)
+	end
+	return newBoard
+end
+
+def visualBoard(boardArray)
+	finalString = ""
+	boardArray.each do |row|
+		i = 0
+		row.each do |place|
+			if i == 9
+				finalString = finalString + "#{place} \n"
+			else
+				finalString = finalString + "#{place} "
+			end
+			i = i + 1
+		end
+	end
+	return finalString
+end
+
+def prompt(*args)
+  print(*args)
+  gets
+end
+
+def generateComputerGuess(playerBoard)
+	newBoard = playerBoard
+	isGoodCoord = false
+	hit = false
+	row = 0
+	coloumn = 0
+	while !isGoodCoord
+		row = rand(10)
+		coloumn = rand(10)
+		if playerBoard[row][coloumn] == "O".colorize(:white)
+			isGoodCoord = true
+			newBoard[row][coloumn] = "-".colorize(:yellow)
+		end
+		if playerBoard[row][coloumn] == "S".colorize(:light_cyan)
+			hit = true
+			isGoodCoord = true
+			newBoard[row][coloumn] = "X".colorize(:red)
+		end
+	end
+	return [row, coloumn, hit, newBoard]
+end
+
+def didLose(board)
+	didLose = true
+	board.each do |row|
+		row.each do |place|
+			if place == "S".colorize(:light_cyan)
+				didLose = false
+			end
+		end
+	end
+	return didLose
+end
+
+def game(board1, board2, board3)
+	playerBoard = board1
+	computerBoard = board2
+	computerDisplayBoard = board3
+	puts "\n \n"
+	puts "Your Board: \n" + visualBoard(playerBoard)
+  puts "\n \n"
+	puts "Opponent's Board: \n" + visualBoard(computerDisplayBoard)
+	coloumn = prompt("Guess a coloumn! ").to_i - 1
+	row = prompt("Guess a row! ").to_i - 1
+	if computerBoard[row][coloumn] == "S".colorize(:light_cyan)
+		puts "\nHit!"
+		computerBoard[row][coloumn] = "X".colorize(:red)
+		computerDisplayBoard[row][coloumn] = "X".colorize(:red)
+	elsif computerBoard[row][coloumn] == "X".colorize(:red) || computerBoard[row][coloumn] == "-".colorize(:yellow)
+		puts "\nYou already guessed this spot."
+	else
+		puts "\nMiss!"
+		computerBoard[row][coloumn] = "-".colorize(:yellow)
+		computerDisplayBoard[row][coloumn] = "-".colorize(:yellow)
+	end
+	puts "Opponent's Board: \n" + visualBoard(computerDisplayBoard)
+	if didLose(computerBoard)
+		puts "\nYOU WON!!!"
+		return " "
+	end
+	sleep(2)
+	computerGuess = generateComputerGuess(playerBoard)
+	playerBoard = computerGuess[3]
+	if computerGuess[2]
+		puts "\nYour opponent hit your ship!"
+	else
+		puts "\nYour opponent missed."
+	end
+	if didLose(playerBoard)
+		puts "\nYou lost. Better luck next time."
+		return " "
+	end
+	game(playerBoard, computerBoard, computerDisplayBoard)
+end
+game(createBoard(1), createBoard(1), createBoard(0))
